4aef96868cce4fffe2905e6dfb18f55a0b065ed7
sidra aslam <aslamsidra876@gmail.com>
2026-03-07 23:16:31 +0500

    new changes

--- vite-project/src/supabaseClient.jsx
+++ vite-project/src/supabaseClient.jsx
@@ -1,6 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://irrxftnykslhxmswvjwv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycnhmdG55a3NsaHhtc3d2and2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMzQ0NzIsImV4cCI6MjA4MjYxMDQ3Mn0.YUM2sELh9LLeI_iF6MKBCHTFz14soGrhY-91xmWj20Y'
+// const supabaseUrl = 'https://irrxftnykslhxmswvjwv.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycnhmdG55a3NsaHhtc3d2and2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMzQ0NzIsImV4cCI6MjA4MjYxMDQ3Mn0.YUM2sELh9LLeI_iF6MKBCHTFz14soGrhY-91xmWj20Y'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// export const supabase = createClient(supabaseUrl, supabaseKey)
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
