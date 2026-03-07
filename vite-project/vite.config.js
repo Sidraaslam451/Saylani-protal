337260ab75cdec656a59ae3fd2d484c4643f2687
sidra aslam <aslamsidra876@gmail.com>
2026-03-07 23:37:46 +0500

    remove tailwind vite plugin

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Add this
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Add this
-  ],
-})
+  plugins: [react()],
+});
